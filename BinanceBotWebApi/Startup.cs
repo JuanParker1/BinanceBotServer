0da269c497551ae688a6f695728a6f9e6e9838ad
cult <septembercult@gmail.com>
2022-02-10 15:03:41 +0500

    Removed Swagger dark theme

--- BinanceBotWebApi/Startup.cs
+++ BinanceBotWebApi/Startup.cs
@@ -50,8 +50,8 @@ namespace BinanceBotWebApi
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "BinanceBotWebApi v1");
-                c.InjectStylesheet("/swagger-ui/SwaggerDark.css");
             });
+            
             if (env.IsDevelopment())
                 app.UseDeveloperExceptionPage();
             else
