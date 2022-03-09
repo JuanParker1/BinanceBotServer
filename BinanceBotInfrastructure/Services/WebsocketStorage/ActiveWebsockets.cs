18532a72bd2ef60d5cb99e340e5041fba8a13801
MachinePoetry <septembercult@gmail.com>
2022-03-09 17:44:34 +0500

    Add: Added users' active websockets storage

---
+++ BinanceBotInfrastructure/Services/WebsocketStorage/ActiveWebsockets.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Concurrent;
+using System.Net.WebSockets;
+
+namespace BinanceBotInfrastructure.Services.WebsocketStorage
+{
+    public class ActiveWebsockets : IActiveWebsockets
+    {
+        private readonly ConcurrentDictionary<int, (ClientWebSocket prices, ClientWebSocket userData)> _activeWebsocketsStorage;
+
+        public ActiveWebsockets()
+        {
+            _activeWebsocketsStorage = new ConcurrentDictionary<int, (ClientWebSocket prices, ClientWebSocket userData)>();
+        }
+
+        public (ClientWebSocket prices, ClientWebSocket userData) Get(int idUser)
+        {
+            var pricesStream = new ClientWebSocket();
+            var userDataStream = new ClientWebSocket();
+            return _activeWebsocketsStorage.GetOrAdd(idUser, (pricesStream, userDataStream));
+        }
+
+        public bool Remove(int idUser)
+        {
+            var (prices, userData) = Get(idUser);
+            prices.Dispose();
+            userData.Dispose();
+            return _activeWebsocketsStorage.TryRemove(idUser, out var result);
+        }
+    }
+}
---
+++ BinanceBotInfrastructure/Services/WebsocketStorage/IActiveWebsockets.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.WebSockets;
+
+namespace BinanceBotInfrastructure.Services.WebsocketStorage
+{
+    public interface IActiveWebsockets
+    {
+        (ClientWebSocket prices, ClientWebSocket userData) Get(int idUser);
+        bool Remove(int idUser);
+    }
+}
